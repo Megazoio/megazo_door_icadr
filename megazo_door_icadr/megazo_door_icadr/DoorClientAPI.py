1738f6b508c01ccdb0328ae5cf3ae8d475c702d0
Megazo-dot-io <hello@megazo.io>
2025-09-30 16:59:21 +0800

    Add files via upload

--- megazo_door_icadr/megazo_door_icadr/DoorClientAPI.py
+++ megazo_door_icadr/megazo_door_icadr/DoorClientAPI.py
@@ -6,20 +6,24 @@ from urllib.error import HTTPError
 namespace = '{http://www.w3.org/1999/xhtml}'
 
 class DoorClientAPI:
+    def __init__(self, node, config):
+        self.name = 'rmf_door_adapter'
+        self.timeout = 5  # seconds
+        self.debug = False
+        self.connected = False
+        self.node = node
+        self.config = config  # use this config to establish connection
 
-    def __init__(self,url,username,password,project_id,ICED_id):
-        self.prefix = url
-        self.timeout = 5.0
-        self.user = username
-        self.passcode = password
-        self.projectID = project_id
-        self.ID = ICED_id
+        self.prefix = self.config["api_endpoint"]
+        self.user = self.config["username"]
+        self.passcode = self.config["password"]
+        self.projectID = self.config["project_id"]
         self.token = ''
 
         count = 0
         self.connected = True
         while not self.check_connection():
-            if count >= 5:
+            if count >= self.timeout:
                 print("Unable to connect to door client API.")
                 self.connected = False
                 break
@@ -28,12 +32,10 @@ class DoorClientAPI:
                 count += 1
             time.sleep(1)
 
-
     def check_connection(self):
         if self.Login():
             if self.ProjectSignIn():
-                if self.check_device_online():
-                    return True
+                return True
         return False
 
 
@@ -81,7 +83,8 @@ class DoorClientAPI:
         return False
 
 
-    def check_device_online(self):
+    def check_device_online(self, door_id) -> int:
+    
         url = self.prefix + '/API/ICED/GetICEDList'
         data = {}
         data['UserID'] = self.user
@@ -93,9 +96,9 @@ class DoorClientAPI:
 
             if data['IsSuccess'] == True:
                 for i in range(len(data['data'])):
-                    if data['data'][i]['ID'] == self.ID:
+                    if data['data'][i]['ICEDName'] == door_id:
                         if data['data'][i]['ControllerStatus'] == 1:
-                            return True
+                            return data['data'][i]['ID']
                         break
             
             print(f'API: Device is offline')
@@ -104,16 +107,16 @@ class DoorClientAPI:
             print(f'HTTP error during check_device_online: {http_err}')
         except Exception as err:
             print(f'Other error during check_device_online: {err}')
-        return False
+        return None
 
 
-    def open_door(self):
+    def open_door(self, iced_id):
         ''' Return True if the door API server is successful receive open door command'''
         url = self.prefix + '/API/Device/ICED/ControlDoor'
         data = {}
         data['UserID'] = self.user
         data['Token'] = self.token
-        data['data'] = {'IDs': [self.ID], 'Operate': 1}
+        data['data'] = {'IDs': [iced_id], 'Operate': 1}
 
         try:
             response = requests.post(url, timeout=self.timeout, json=data)
@@ -131,13 +134,13 @@ class DoorClientAPI:
         return False
 
 
-    def close_door(self):
+    def close_door(self, iced_id):
         ''' Return True if the door API server is successful receive open door command'''
         url = self.prefix + '/API/Device/ICED/ControlDoor'
         data = {}
         data['UserID'] = self.user
         data['Token'] = self.token
-        data['data'] = {'IDs': [self.ID], 'Operate': 2}
+        data['data'] = {'IDs': [iced_id], 'Operate': 2}
 
         try:
             response = requests.post(url, timeout=self.timeout, json=data)
@@ -153,40 +156,6 @@ class DoorClientAPI:
         except Exception as err:
             print(f'Other error during Close Door: {err}')
         return False
-
-
-    # NOT IN USE. USING MQTT TO GET STATUS.
-    def get_mode(self):        
-        ''' Return the door status with reference rmf_door_msgs. 
-            Return DoorMode.MODE_CLOSED when door status is closed.
-            Return DoorMode.MODE_MOVING when door status is moving.
-            Return DoorMode.MODE_OPEN when door status is open.
-            Return DoorMode.MODE_OFFLINE when door status is offline.
-            Return DoorMode.MODE_UNKNOWN when door status is unknown'''
-        url = self.prefix + '/API/ICED/GetICEDList'
-        data = {}
-        data['UserID'] = self.user
-        data['Token'] = self.token
-
-        try:
-            response = requests.post(url, timeout=self.timeout, json=data)
-            data = response.json()
-
-            if data['IsSuccess'] == True:
-                for i in range(len(data['data'])):
-                    if data['data'][i]['ID'] == self.ID:
-                        if data['data'][i]['DoorOpenStatus'] == 1:
-                            return DoorMode.MODE_OPEN
-                        else:
-                            return DoorMode.MODE_CLOSED
-            else:
-                print(f'API: Door get_mode Failed')
-
-        except HTTPError as http_err:
-            print(f'HTTP error during Door get_mode: {http_err}')
-        except Exception as err:
-            print(f'Other error during Door get_mode: {err}')
-        return DoorMode.MODE_UNKNOWN
 
 
     def sys_ping(self):
@@ -209,6 +178,4 @@ class DoorClientAPI:
         except Exception as err:
             print(f'Other error during System Ping: {err}')
         return False  
-
-        
 
--- megazo_door_icadr/megazo_door_icadr/door_adapter.py
+++ megazo_door_icadr/megazo_door_icadr/door_adapter.py
@@ -8,13 +8,30 @@ import threading
 import rclpy
 from megazo_door_icadr.DoorClientAPI import DoorClientAPI
 from rclpy.node import Node
-from rclpy.time import Time
 from rmf_door_msgs.msg import DoorRequest, DoorState, DoorMode
+
 import paho.mqtt.client as mqtt
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
 import json
 
+
+class Door:
+    def __init__(self,
+                 id,
+                 door_auto_closes,
+                 door_signal_period,
+                 continuous_status_polling,
+                 iced_id):
+        self.id = id
+        self.door_mode = DoorMode.MODE_CLOSED
+        self.open_door = False
+        self.check_status = None  # set to None if not enabled
+        self.door_auto_closes = door_auto_closes
+        self.door_signal_period = door_signal_period
+        self.iced_id = iced_id
+        if continuous_status_polling:
+            self.check_status = False
 
 ###############################################################################
 
@@ -24,45 +41,60 @@ class DoorAdapter(Node):
         self.get_logger().info('Starting door adapter...')
 
         # Get value from config file
-        self.door_name = config_yaml['door']['name']
-        self.door_close_feature = config_yaml['door']['door_close_feature']
-        self.door_signal_period = config_yaml['door']['door_signal_period']
         self.door_state_publish_period = config_yaml['door_publisher']['door_state_publish_period']
-        
-        url = config_yaml['door']['api_endpoint']
-        mqtt_endpoint = config_yaml['door']['mqtt_endpoint']
-        mqtt_port = config_yaml['door']['mqtt_port']
-        mqtt_topic = config_yaml['door']['mqtt_topic']
-        username = config_yaml['door']['username']
-        password = config_yaml['door']['password']
-        mqtt_password = config_yaml['door']['mqtt_password']
-        mqtt_client_id = config_yaml['door']['mqtt_client_id']
-        project_id = config_yaml['door']['project_id']
-        ICED_id = config_yaml['door']['ICED_id']
-        
+
         door_pub = config_yaml['door_publisher']
         door_sub = config_yaml['door_subscriber']
+        self.mock_adapter = config_yaml.get('mock', False)
 
-        self.api = DoorClientAPI(url,username,password,project_id,ICED_id)
-       
-        assert self.api.connected, "Unable to establish connection with door"
-        
-        # default door state - closed mode
-        self.door_mode = DoorMode.MODE_CLOSED
-        # open door flag
-        self.open_door = False
-        self.check_status = True #ensure initial door state is correct
-        
+        # Connect to doors
+        if not self.mock_adapter:
+            self.api = DoorClientAPI(self, config_yaml)
+
+            assert self.api.connected, "Unable to establish connection with door"
+
+            # Keep track of doors
+            self.doors = {}
+            for door_id, door_data in config_yaml['doors'].items():
+                # We support both door_auto_closes and the deprecated
+                # door_close_feature for backward compatibility
+                auto_close = door_data.get('door_auto_closes', None)
+                if auto_close is None:
+                    if 'door_close_feature' in door_data:
+                        auto_close = not door_data['door_close_feature']
+                assert auto_close is not None
+
+                iced_id = self.api.check_device_online(door_id)
+
+                assert iced_id is not None, "Megazo IoT is not online for [{door_id}]"
+                self.get_logger().info(f"Megazo device ID [{iced_id}] for [{door_id}] is online")
+
+                self.doors[door_id] = Door(door_id,
+                                           auto_close,
+                                           door_data['door_signal_period'],
+                                           door_data.get('continuous_status_polling', False),
+                                           iced_id)
+                
         self.door_states_pub = self.create_publisher(
-            DoorState, door_pub['topic_name'], 10)
+            DoorState, door_pub['topic_name'], 100)
 
         self.door_request_sub = self.create_subscription(
-            DoorRequest, door_sub['topic_name'], self.door_request_cb, 10)
+            DoorRequest, door_sub['topic_name'], self.door_request_cb, 100)
 
         self.periodic_timer = self.create_timer(
             self.door_state_publish_period, self.time_cb)
 
         #MQTT
+
+        # Get value from config file
+        mqtt_endpoint = config_yaml['mqtt_endpoint']
+        mqtt_port = config_yaml['mqtt_port']
+        mqtt_topic = config_yaml['mqtt_topic']
+        username = config_yaml['username']
+        mqtt_password = config_yaml['mqtt_password']
+        mqtt_client_id = config_yaml['mqtt_client_id']
+        project_id = config_yaml['project_id']
+
         # Create the MQTT client instance
         client = mqtt.Client(client_id=mqtt_client_id, protocol=mqtt.MQTTv5, transport='websockets')
         client.username_pw_set(username,mqtt_password)
@@ -89,19 +121,45 @@ class DoorAdapter(Node):
 
         client.loop_start() #start the loop
 
+        #publish initial door state
+        for door_id, door_data in self.doors.items():
+
+            state_msg = DoorState()
+            state_msg.door_time = self.get_clock().now().to_msg()
+
+            # publish states of the door
+            state_msg.door_name = door_id
+            state_msg.current_mode.value = door_data.door_mode
+            self.door_states_pub.publish(state_msg)
+
+            self.get_logger().info(f"Initializing door states for [{door_id}]")
+            
 
     def on_message_mqtt(self, client, userdata, msg):
         #print(msg.payload)
         decoded_message=str(msg.payload.decode("utf-8"))
         msg_=json.loads(decoded_message)
         mqttEvent = msg_['doorEvent']['EventType']
+        mqttId = msg_['doorEvent']['ControllerName']
         self.get_logger().info(f"New MQTT Event: = {mqttEvent}")
         self.get_logger().info(f"{msg_}")
 
-        if mqttEvent == 3:
-            self.door_mode = DoorMode.MODE_CLOSED
-        elif mqttEvent == 4 or mqttEvent == 14:
-            self.door_mode = DoorMode.MODE_OPEN
+        for door_id, door_data in self.doors.items():
+
+            #Publish only the status of the doors indicated in the config file
+            if door_id == mqttId:
+                if mqttEvent == 3:
+                    door_data.door_mode = DoorMode.MODE_CLOSED
+                elif mqttEvent == 4 or mqttEvent == 14:
+                    door_data.door_mode = DoorMode.MODE_OPEN
+
+                state_msg = DoorState()
+                state_msg.door_time = self.get_clock().now().to_msg()
+
+                # publish states of the door
+                state_msg.door_name = door_id
+                state_msg.current_mode.value = door_data.door_mode
+                self.door_states_pub.publish(state_msg)
 
 
     def on_connect_mqtt(self, client, userdata, flags, rc, properties):
@@ -111,50 +169,69 @@ class DoorAdapter(Node):
             self.get_logger().error(f"Unable to connect to MQTT Broker. Returned Code = {rc}")
 
 
-    def door_open_command_request(self):
-        while self.open_door:
-            success = self.api.open_door()
+    def door_open_command_request(self, door_data: Door):
+        # assume API doesn't have close door API
+        # Once the door command is posted to the door API,
+        # the door will be opened and then close after 5 secs    
+        while door_data.open_door:
+            success = self.api.open_door(door_data.iced_id)
             if success:
-                self.get_logger().info(f"Request to open door [{self.door_name}] is successful")
+                self.get_logger().info(f"Request to open door [{door_data.id}] is successful")
             else:
-                self.get_logger().warning(f"Request to open door [{self.door_name}] is unsuccessful")
-            time.sleep(self.door_signal_period)
-
+                self.get_logger().warning(f"Request to open door [{door_data.id}] is unsuccessful")
+            time.sleep(door_data.door_signal_period)
 
     def time_cb(self):
+        if self.mock_adapter:
+            return
         assert self.api.sys_ping(), "Unable to ping connection with door"
-        state_msg = DoorState()
-        state_msg.door_time = self.get_clock().now().to_msg()
-
-        # publish states of the door
-        state_msg.door_name = self.door_name
-        state_msg.current_mode.value = self.door_mode
-        self.door_states_pub.publish(state_msg)
 
 
     def door_request_cb(self, msg: DoorRequest):
-        # when door node receive open request, the door adapter will send open command to API
-        # If door node receive close request, the door adapter will stop sending open command to API
-        # check DoorRequest msg whether the door name of the request is same as the current door. If not, ignore the request
-        if msg.door_name == self.door_name:
-            self.get_logger().info(f"Door mode [{msg.requested_mode.value}] requested by {msg.requester_id}")
-            if msg.requested_mode.value == DoorMode.MODE_OPEN:
-                # open door implementation
-                self.open_door = True
-                self.check_status = True
-                if self.door_close_feature:
-                    self.api.open_door()
-                else:
-                    t = threading.Thread(target = self.door_open_command_request)
-                    t.start()
-            elif msg.requested_mode.value == DoorMode.MODE_CLOSED:
-                # close door implementation
-                self.open_door = False
-                self.get_logger().info('Close Command to door received')
-                if self.door_close_feature:
-                    self.api.close_door()
+        # Agree to every request automatically if this is a mock adapter
+        if self.mock_adapter:
+            state_msg = DoorState()
+            state_msg.door_time = self.get_clock().now().to_msg()
+            state_msg.door_name = msg.door_name
+            state_msg.current_mode.value = msg.requested_mode.value
+            self.door_states_pub.publish(state_msg)
+            return
+
+        # Check if this door has been stored in the door adapter. If not, ignore
+        door_data = self.doors.get(msg.door_name)
+        if door_data is None:
+            return
+
+        # When the adapter receives an open request, it will send an open
+        # command to API. When the adapter receives a close request, it will
+        # stop sending the open command to API
+        self.get_logger().info(
+            f"[{msg.door_name}] Door mode [{msg.requested_mode.value}] "
+            f"requested by {msg.requester_id}"
+        )
+        if msg.requested_mode.value == DoorMode.MODE_OPEN:
+            # open door implementation
+            door_data.open_door = True
+            if door_data.check_status is not None:
+                # If check_status is enabled, we toggle it to true to allow
+                # door state updates
+                door_data.check_status = True
+            if not door_data.door_auto_closes:
+                self.get_logger().info(f'Calling [{msg.door_name}] ICED ID [{door_data.iced_id}] open door API')
+                self.api.open_door(door_data.iced_id)
             else:
-                self.get_logger().error('Invalid door mode requested. Ignoring...')
+                t = threading.Thread(target=self.door_open_command_request,
+                                     args=(door_data,))
+                t.start()
+        elif msg.requested_mode.value == DoorMode.MODE_CLOSED:
+            # close door implementation
+            door_data.open_door = False
+            self.get_logger().info(f'[{msg.door_name}] Close Command to door received')
+            if not door_data.door_auto_closes:
+                self.get_logger().info(f'Calling [{msg.door_name}] ICED ID [{door_data.iced_id}] close door API')
+                self.api.close_door(door_data.iced_id)
+        else:
+            self.get_logger().error('Invalid door mode requested. Ignoring...')
 
 ###############################################################################
 
