ab74b9fb855d5b87522f05301a64a0a663f6fc93
gary.bey <gary.bey@kabam.ai>
2025-12-17 21:55:00 +0800

    :hammer: Enabled colorised docker logs when running dev script for running package. Easier for debugging.
    Signed-off-by: gary.bey <gary.bey@kabam.ai>

--- scripts/2_run_docker_container.bash
+++ scripts/2_run_docker_container.bash
@@ -3,4 +3,5 @@
 docker run -it --rm \
 	--name door_adapter_megazo_c \
 	--network host \
+	-e RCUTILS_COLORIZED_OUTPUT=1 \
 door_adapter_megazo:icadr bash -c "source /ros_entrypoint.sh && ros2 launch door_adapter_megazo run.launch.xml"
