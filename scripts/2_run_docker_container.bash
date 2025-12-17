24691d1911bdce72d62ca451ec092dec1dcc3a8f
gary.bey <gary.bey@kabam.ai>
2025-12-17 22:16:16 +0800

    :hammer: Added quick toggle for showing and hiding debug log statements.
    Signed-off-by: gary.bey <gary.bey@kabam.ai>

--- scripts/2_run_docker_container.bash
+++ scripts/2_run_docker_container.bash
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+CONFIG_PATH="/home/megazo_door_ws/config.yaml"
+
+echo -n "Enable DEBUG logging? (y/N): "
+read answer
+
+# Default to INFO if empty or not y/Y
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    LOG_LEVEL="DEBUG"
+else
+    LOG_LEVEL="INFO"
+fi
+
+echo "Selected log level: $LOG_LEVEL"
+
 docker run -it --rm \
 	--name door_adapter_megazo_c \
 	--network host \
 	-e RCUTILS_COLORIZED_OUTPUT=1 \
-door_adapter_megazo:icadr bash -c "source /ros_entrypoint.sh && ros2 launch door_adapter_megazo run.launch.xml"
+door_adapter_megazo:icadr bash -c "source /ros_entrypoint.sh && \
+ros2 launch door_adapter_megazo run.launch.xml \
+log_level:=$LOG_LEVEL"
