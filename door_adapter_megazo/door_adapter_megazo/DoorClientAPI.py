7010bf314ae533ac15a0168d8e450395bfab824e
gary.bey <gary.bey@kabam.ai>
2025-12-17 21:36:31 +0800

    :hammer: :heavy_plus_sign: Added dev scripts for quick test of open and close door via ROS 2 interactions.
    Signed-off-by: gary.bey <gary.bey@kabam.ai>

--- door_adapter_megazo/door_adapter_megazo/DoorClientAPI.py
+++ door_adapter_megazo/door_adapter_megazo/DoorClientAPI.py
@@ -6,36 +6,35 @@ from urllib.error import HTTPError
 namespace = '{http://www.w3.org/1999/xhtml}'
 
 class DoorClientAPI:
-    def __init__(self, node, config):
-        self.name = 'rmf_door_adapter'
-        self.timeout = 5  # seconds
-        self.debug = False
-        self.connected = False
-        self.node = node
-        self.config = config  # use this config to establish connection
 
-        self.prefix = self.config["api_endpoint"]
-        self.user = self.config["username"]
-        self.passcode = self.config["password"]
-        self.projectID = self.config["project_id"]
+    def __init__(self, url, username, password, project_id, ICED_id, logger):
+        self.prefix = url
+        self.timeout = 5.0
+        self.user = username
+        self.passcode = password
+        self.projectID = project_id
+        self.ID = ICED_id
         self.token = ''
+        self.logger = logger
 
         count = 0
         self.connected = True
         while not self.check_connection():
-            if count >= self.timeout:
-                print("Unable to connect to door client API.")
+            if count >= 5:
+                self.logger.error("Unable to connect to door client API.")
                 self.connected = False
                 break
             else:
-                print("Unable to connect to door client API. Attempting to reconnect...")
+                self.logger.warn("Unable to connect to door client API. Attempting to reconnect...")
                 count += 1
             time.sleep(1)
+
 
     def check_connection(self):
         if self.Login():
             if self.ProjectSignIn():
-                return True
+                if self.check_device_online():
+                    return True
         return False
 
 
@@ -52,21 +51,24 @@ class DoorClientAPI:
                 self.token = data['data']['Token']
                 return True
             else:
-                print(f'API:Login Failed')
+                self.logger.error(f"RAW - data: {data}")
+                self.logger.error(f'API:Login Failed')
 
         except HTTPError as http_err:
-            print(f'HTTP error during Login: {http_err}')
+            self.logger.error(f'HTTP error during Login: {http_err}')
         except Exception as err:
-            print(f'Other error during Login: {err}')
+            self.logger.error(f'Other error during Login: {err}')
         return False
 
 
     def ProjectSignIn(self):
         url = self.prefix + '/API/System/ProjectSignIn'
+        self.logger.info(f"url = {url}")
         data = {}
         data['UserID'] = self.user
         data['Token'] = self.token
         data['data'] = {'ProjectID': self.projectID}
+        self.logger.info(f"payload = {data}")
         try:
             response = requests.post(url, timeout=self.timeout, json=data)
             data = response.json()
@@ -74,17 +76,17 @@ class DoorClientAPI:
             if data['IsSuccess'] == True:
                 return True
             else:
-                print(f'API:Project Sign-in Failed')
+                self.logger.error(f"data = {data}")
+                self.logger.error(f'API:Project Sign-in Failed')
 
         except HTTPError as http_err:
-            print(f'HTTP error during Project Sign-in: {http_err}')
+            self.logger.error(f'HTTP error during Project Sign-in: {http_err}')
         except Exception as err:
-            print(f'Other error during Project Sign-in: {err}')
+            self.logger.error(f'Other error during Project Sign-in: {err}')
         return False
 
 
-    def check_device_online(self, door_id) -> int:
-    
+    def check_device_online(self):
         url = self.prefix + '/API/ICED/GetICEDList'
         data = {}
         data['UserID'] = self.user
@@ -96,27 +98,27 @@ class DoorClientAPI:
 
             if data['IsSuccess'] == True:
                 for i in range(len(data['data'])):
-                    if data['data'][i]['ICEDName'] == door_id:
+                    if data['data'][i]['ID'] == self.ID:
                         if data['data'][i]['ControllerStatus'] == 1:
-                            return data['data'][i]['ID']
+                            return True
                         break
             
-            print(f'API: Device is offline')
+            self.logger.error(f'API: Device is offline')
                 
         except HTTPError as http_err:
-            print(f'HTTP error during check_device_online: {http_err}')
+            self.logger.error(f'HTTP error during check_device_online: {http_err}')
         except Exception as err:
-            print(f'Other error during check_device_online: {err}')
-        return None
+            self.logger.error(f'Other error during check_device_online: {err}')
+        return False
 
 
-    def open_door(self, iced_id):
+    def open_door(self):
         ''' Return True if the door API server is successful receive open door command'''
         url = self.prefix + '/API/Device/ICED/ControlDoor'
         data = {}
         data['UserID'] = self.user
         data['Token'] = self.token
-        data['data'] = {'IDs': [iced_id], 'Operate': 1}
+        data['data'] = {'IDs': [self.ID], 'Operate': 1}
 
         try:
             response = requests.post(url, timeout=self.timeout, json=data)
@@ -125,22 +127,22 @@ class DoorClientAPI:
             if data['IsSuccess'] == True:
                 return True
             else:
-                print(f'API: Open Door Failed')
+                self.logger.error(f'API: Open Door Failed')
 
         except HTTPError as http_err:
-            print(f'HTTP error during Open Door: {http_err}')
+            self.logger.error(f'HTTP error during Open Door: {http_err}')
         except Exception as err:
-            print(f'Other error during Open Door: {err}')
+            self.logger.error(f'Other error during Open Door: {err}')
         return False
 
 
-    def close_door(self, iced_id):
+    def close_door(self):
         ''' Return True if the door API server is successful receive open door command'''
         url = self.prefix + '/API/Device/ICED/ControlDoor'
         data = {}
         data['UserID'] = self.user
         data['Token'] = self.token
-        data['data'] = {'IDs': [iced_id], 'Operate': 2}
+        data['data'] = {'IDs': [self.ID], 'Operate': 2}
 
         try:
             response = requests.post(url, timeout=self.timeout, json=data)
@@ -149,13 +151,47 @@ class DoorClientAPI:
             if data['IsSuccess'] == True:
                 return True
             else:
-                print(f'API: Close Door Failed')
+                self.logger.error(f'API: Close Door Failed')
 
         except HTTPError as http_err:
-            print(f'HTTP error during Close Door: {http_err}')
+            self.logger.error(f'HTTP error during Close Door: {http_err}')
         except Exception as err:
-            print(f'Other error during Close Door: {err}')
+            self.logger.error(f'Other error during Close Door: {err}')
         return False
+
+
+    # NOT IN USE. USING MQTT TO GET STATUS.
+    def get_mode(self):        
+        ''' Return the door status with reference rmf_door_msgs. 
+            Return DoorMode.MODE_CLOSED when door status is closed.
+            Return DoorMode.MODE_MOVING when door status is moving.
+            Return DoorMode.MODE_OPEN when door status is open.
+            Return DoorMode.MODE_OFFLINE when door status is offline.
+            Return DoorMode.MODE_UNKNOWN when door status is unknown'''
+        url = self.prefix + '/API/ICED/GetICEDList'
+        data = {}
+        data['UserID'] = self.user
+        data['Token'] = self.token
+
+        try:
+            response = requests.post(url, timeout=self.timeout, json=data)
+            data = response.json()
+
+            if data['IsSuccess'] == True:
+                for i in range(len(data['data'])):
+                    if data['data'][i]['ID'] == self.ID:
+                        if data['data'][i]['DoorOpenStatus'] == 1:
+                            return DoorMode.MODE_OPEN
+                        else:
+                            return DoorMode.MODE_CLOSED
+            else:
+                self.logger.error(f'API: Door get_mode Failed')
+
+        except HTTPError as http_err:
+            self.logger.error(f'HTTP error during Door get_mode: {http_err}')
+        except Exception as err:
+            self.logger.error(f'Other error during Door get_mode: {err}')
+        return DoorMode.MODE_UNKNOWN
 
 
     def sys_ping(self):
@@ -171,11 +207,10 @@ class DoorClientAPI:
             if data['IsSuccess'] == True:
                 return True
             else:
-                print(f'API: System Ping Failed')
+                self.logger.error(f'API: System Ping Failed')
 
         except HTTPError as http_err:
-            print(f'HTTP error during System Ping: {http_err}')
+            self.logger.error(f'HTTP error during System Ping: {http_err}')
         except Exception as err:
-            print(f'Other error during System Ping: {err}')
-        return False  
-
+            self.logger.error(f'Other error during System Ping: {err}')
+        return False 
