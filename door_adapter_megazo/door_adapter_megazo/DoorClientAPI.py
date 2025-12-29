3b353f73a81ec7ffb36fd36b8eabf309101fd0d3
gary.bey <gary.bey@kabam.ai>
2025-12-29 21:42:47 +0800

    :hammer: Identified and labelled private class variables.
    Signed-off-by: gary.bey <gary.bey@kabam.ai>

--- door_adapter_megazo/door_adapter_megazo/DoorClientAPI.py
+++ door_adapter_megazo/door_adapter_megazo/DoorClientAPI.py
@@ -29,30 +29,30 @@ class DoorClientAPI:
             devices,
             logger):
 
-        self.prefix = url
-        self.timeout = 5.0
-        self.user = username
-        self.passcode = password
-        self.projectID = project_id
-        self.devices = devices
-        self.token = ''
-        self.logger = logger
+        self._token = ''
+        self._timeout = 5.0
+        self._logger = logger
+        self._prefix = url
+        self._user = username
+        self._passcode = password
+        self._projectID = project_id
+        self._devices = devices
 
         count = 0
         self.connected = True
         while not self._check_connection():
             if count >= 5:
-                self.logger.error('Unable to connect to door client '
-                                  'API after 5 attempts. Exiting...')
+                self._logger.error('Unable to connect to door client '
+                                   'API after 5 attempts. Exiting...')
                 self.connected = False
                 break
             else:
-                self.logger.warn('Unable to connect to door client '
-                                 f'API. Attempting to reconnect [{str(count)}/5]...')
+                self._logger.warn('Unable to connect to door client '
+                                  f'API. Attempting to reconnect [{str(count)}/5]...')
                 count += 1
             time.sleep(1)
 
-    def _check_connection(self):
+    def _check_connection(self) -> bool:
         if self._login():
             if self._signin_project():
                 if self.check_devices_online():
@@ -61,143 +61,143 @@ class DoorClientAPI:
 
     def _login(self) -> bool:
         """Return True if authentication token has been retrieved successfully."""
-        url = self.prefix + '/API/System/Login'
+        url = self._prefix + '/API/System/Login'
         data = {}
-        data['data'] = {'UserID': self.user, 'Password': self.passcode}
+        data['data'] = {'UserID': self._user, 'Password': self._passcode}
 
         try:
-            response = requests.post(url, timeout=self.timeout, json=data)
+            response = requests.post(url, timeout=self._timeout, json=data)
             data = response.json()
 
             if data['IsSuccess'] is True:
-                self.token = data['data']['Token']
+                self._token = data['data']['Token']
                 return True
             else:
-                self.logger.error(f'RAW - data: {data}')
-                self.logger.error('API:Login Failed')
+                self._logger.error(f'RAW - data: {data}')
+                self._logger.error('API:Login Failed')
 
         except HTTPError as http_err:
-            self.logger.error(f'HTTP error during Login: {http_err}')
+            self._logger.error(f'HTTP error during Login: {http_err}')
         except Exception as err:
-            self.logger.error(f'Other error during Login: {err}')
+            self._logger.error(f'Other error during Login: {err}')
         return False
 
     def _signin_project(self) -> bool:
         """Return True if specific project has been successfully signed into."""
-        url = self.prefix + '/API/System/ProjectSignIn'
+        url = self._prefix + '/API/System/ProjectSignIn'
         data = {}
-        data['UserID'] = self.user
-        data['Token'] = self.token
-        data['data'] = {'ProjectID': self.projectID}
-        self.logger.debug(f'payload = {data}')
+        data['UserID'] = self._user
+        data['Token'] = self._token
+        data['data'] = {'ProjectID': self._projectID}
+        self._logger.debug(f'payload = {data}')
         try:
-            response = requests.post(url, timeout=self.timeout, json=data)
+            response = requests.post(url, timeout=self._timeout, json=data)
             data = response.json()
 
             if data['IsSuccess'] is True:
                 return True
             else:
-                self.logger.error(f'data = {data}')
-                self.logger.error('API:Project Sign-in Failed')
+                self._logger.error(f'data = {data}')
+                self._logger.error('API:Project Sign-in Failed')
 
         except HTTPError as http_err:
-            self.logger.error(f'HTTP error during Project Sign-in: {http_err}')
+            self._logger.error(f'HTTP error during Project Sign-in: {http_err}')
         except Exception as err:
-            self.logger.error(f'Other error during Project Sign-in: {err}')
+            self._logger.error(f'Other error during Project Sign-in: {err}')
         return False
 
-    def check_devices_online(self):
+    def check_devices_online(self) -> bool:
         """Return True if all devices in project is online."""
-        url = self.prefix + '/API/ICED/GetICEDList'
+        url = self._prefix + '/API/ICED/GetICEDList'
         data = {}
-        data['UserID'] = self.user
-        data['Token'] = self.token
+        data['UserID'] = self._user
+        data['Token'] = self._token
 
         try:
-            response = requests.post(url, timeout=self.timeout, json=data)
+            response = requests.post(url, timeout=self._timeout, json=data)
             data = response.json()
 
             if data['IsSuccess'] is True:
                 for i in range(len(data['data'])):
-                    for _, device_data in self.devices.items():
+                    for _, device_data in self._devices.items():
                         if data['data'][i]['ID'] == device_data['ICED_id']:
                             if data['data'][i]['ControllerStatus'] == 0:
-                                self.logger.error('API: Device '
-                                                  '[ {data["data"][i]["ICEDName"]} ] - '
-                                                  '[ OFFLINE ]')
+                                self._logger.error('API: Device '
+                                                   '[ {data["data"][i]["ICEDName"]} ] - '
+                                                   '[ OFFLINE ]')
                                 return False
                             break
         except HTTPError as http_err:
-            self.logger.error(f'HTTP error during check_devices_online: {http_err}')
+            self._logger.error(f'HTTP error during check_devices_online: {http_err}')
         except Exception as err:
-            self.logger.error(f'Other error during check_devices_online: {err}')
+            self._logger.error(f'Other error during check_devices_online: {err}')
 
         return True
 
     def open_door(self, device_ICED_id):
         """Return True if the door API server is successful receive open door command."""
-        url = self.prefix + '/API/Device/ICED/ControlDoor'
+        url = self._prefix + '/API/Device/ICED/ControlDoor'
         data = {}
-        data['UserID'] = self.user
-        data['Token'] = self.token
+        data['UserID'] = self._user
+        data['Token'] = self._token
         data['data'] = {'IDs': [device_ICED_id], 'Operate': 1}
 
         try:
-            response = requests.post(url, timeout=self.timeout, json=data)
+            response = requests.post(url, timeout=self._timeout, json=data)
             data = response.json()
 
             if data['IsSuccess'] is True:
                 return True
             else:
-                self.logger.error('API: Open Door Failed')
+                self._logger.error('API: Open Door Failed')
 
         except HTTPError as http_err:
-            self.logger.error(f'HTTP error during Open Door: {http_err}')
+            self._logger.error(f'HTTP error during Open Door: {http_err}')
         except Exception as err:
-            self.logger.error(f'Other error during Open Door: {err}')
+            self._logger.error(f'Other error during Open Door: {err}')
         return False
 
     def close_door(self, device_ICED_id):
         """Return True if the door API server is successful receive open door command."""
-        url = self.prefix + '/API/Device/ICED/ControlDoor'
+        url = self._prefix + '/API/Device/ICED/ControlDoor'
         data = {}
-        data['UserID'] = self.user
-        data['Token'] = self.token
+        data['UserID'] = self._user
+        data['Token'] = self._token
         data['data'] = {'IDs': [device_ICED_id], 'Operate': 2}
 
         try:
-            response = requests.post(url, timeout=self.timeout, json=data)
+            response = requests.post(url, timeout=self._timeout, json=data)
             data = response.json()
 
             if data['IsSuccess'] is True:
                 return True
             else:
-                self.logger.error('API: Close Door Failed')
+                self._logger.error('API: Close Door Failed')
 
         except HTTPError as http_err:
-            self.logger.error(f'HTTP error during Close Door: {http_err}')
+            self._logger.error(f'HTTP error during Close Door: {http_err}')
         except Exception as err:
-            self.logger.error(f'Other error during Close Door: {err}')
+            self._logger.error(f'Other error during Close Door: {err}')
         return False
 
     def ping_system(self) -> bool:
         """Return True if ICADR system was pinged for token refreshing."""
-        url = self.prefix + '/API/SYSTEM/Ping'
+        url = self._prefix + '/API/SYSTEM/Ping'
         data = {}
-        data['UserID'] = self.user
-        data['Token'] = self.token
+        data['UserID'] = self._user
+        data['Token'] = self._token
 
         try:
-            response = requests.post(url, timeout=self.timeout, json=data)
+            response = requests.post(url, timeout=self._timeout, json=data)
             data = response.json()
 
             if data['IsSuccess'] is True:
                 return True
             else:
-                self.logger.error('API: System Ping Failed')
+                self._logger.error('API: System Ping Failed')
 
         except HTTPError as http_err:
-            self.logger.error(f'HTTP error during System Ping: {http_err}')
+            self._logger.error(f'HTTP error during System Ping: {http_err}')
         except Exception as err:
-            self.logger.error(f'Other error during System Ping: {err}')
+            self._logger.error(f'Other error during System Ping: {err}')
         return False
